aa87d19de71f9cb92fe80d11c4221d1e6d820259
Will-Ribeiro00 <william.ribeiro1403@gmail.com>
2024-01-02 14:30:21 -0300

    Desafio Concluido!

--- TestesUnitarios-Desafio/TestesUnitarios-Desafio/Services/ValidacoesString.cs
+++ TestesUnitarios-Desafio/TestesUnitarios-Desafio/Services/ValidacoesString.cs
@@ -1,4 +1,10 @@
-namespace TestesUnitarios.Desafio.Console.Services
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestesUnitarios_Desafio.Services
 {
     public class ValidacoesString
     {
